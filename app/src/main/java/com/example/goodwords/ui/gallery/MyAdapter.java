000a068ac673be9f0d22456122271a14ae867e16
jwj6542 <jwj6542@naver.com>
2020-05-24 11:53:52 +0900

    Toast finish

--- app/src/main/java/com/example/goodwords/ui/gallery/MyAdapter.java
+++ app/src/main/java/com/example/goodwords/ui/gallery/MyAdapter.java
@@ -15,7 +15,15 @@ import com.example.goodwords.R;
 import java.util.ArrayList;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
-    private ArrayList<Gallery> mMovieData = null;
+    private ArrayList<Gallery> mGalleryData = null;
+    public interface OnItemClickListener{
+        void  onItemClick(View v,Gallery gallery);
+    }
+    private OnItemClickListener mListener = null;
+    public void setOnItemClickListener(OnItemClickListener listener){
+        this.mListener = listener;
+
+    }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         ImageView imageView;
@@ -23,13 +31,25 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int pos = getAdapterPosition();
+                    if (pos != RecyclerView.NO_POSITION){
+                        if (mListener !=null){
+                            mListener.onItemClick(v,mGalleryData.get(pos));
+                        }
+                    }
+
+                }
+            });
             imageView = itemView.findViewById(R.id.iv_item);
             textView = itemView.findViewById(R.id.tv_item);
         }
     }
 
     public MyAdapter(ArrayList<Gallery> movieList) {
-        mMovieData = movieList;
+        mGalleryData = movieList;
     }
 
 
@@ -47,12 +67,12 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull MyAdapter.ViewHolder holder, int position) {
-        holder.imageView.setImageDrawable(mMovieData.get(position).getDrawable());
+        holder.imageView.setImageDrawable(mGalleryData.get(position).getDrawable());
 
     }
 
     @Override
     public int getItemCount() {
-        return mMovieData.size();
+        return mGalleryData.size();
     }
 }
