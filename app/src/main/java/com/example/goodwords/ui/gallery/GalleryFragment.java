691d1fa37033eb62889a744e1d0bc4017643fbc0
dudwns0911 <dudwns0911@naver.com>
2020-06-07 12:30:21 +0900

    imageView edit

--- app/src/main/java/com/example/goodwords/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/goodwords/ui/gallery/GalleryFragment.java
@@ -45,6 +45,22 @@ public class GalleryFragment extends Fragment {
         sGoodWords.add(new Gallery(getActivity().getDrawable(R.drawable.drawable104), "믿음"));
         sGoodWords.add(new Gallery(getActivity().getDrawable(R.drawable.drawable105), "논리, 좋은말,성격,자기개발,자아성찰"));
         sGoodWords.add(new Gallery(getActivity().getDrawable(R.drawable.drawable106), "자기개발,자아성찰,좋은말,미래"));
+        sGoodWords.add(new Gallery(getActivity().getDrawable(R.drawable.drawable107), "고난"));
+        sGoodWords.add(new Gallery(getActivity().getDrawable(R.drawable.drawable108), "인간"));
+        sGoodWords.add(new Gallery(getActivity().getDrawable(R.drawable.drawable109), "사랑, 부족"));
+        sGoodWords.add(new Gallery(getActivity().getDrawable(R.drawable.drawable110), "꿈, 목표, 계획, 실행"));
+        sGoodWords.add(new Gallery(getActivity().getDrawable(R.drawable.drawable111), "천재,영감,땀"));
+        sGoodWords.add(new Gallery(getActivity().getDrawable(R.drawable.drawable112), "일, 놀이"));
+        sGoodWords.add(new Gallery(getActivity().getDrawable(R.drawable.drawable113), "성공, 사람"));
+        sGoodWords.add(new Gallery(getActivity().getDrawable(R.drawable.drawable114), "자기반성, 지혜"));
+        sGoodWords.add(new Gallery(getActivity().getDrawable(R.drawable.drawable115), "돈, 삶, 도구"));
+        sGoodWords.add(new Gallery(getActivity().getDrawable(R.drawable.drawable116), "사람"));
+
+
+
+
+
+
 
 
         RecyclerView recyclerView = view.findViewById(R.id.recycler1);
